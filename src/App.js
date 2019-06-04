1c0e2a1237a6868a0b81a02353e58fb766d1213f
tallan groberg <tallan.taven@gmail.com>
2019-06-04 14:36:42 -0600

    render card fronts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { Container, Header, } from "semantic-ui-react"; 
+import { Container, } from "semantic-ui-react";
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    faces: [
+      { id: 1, front: "this front", back: "this back", },
+      { id: 2, front: "this 2 front", back: "this 2 back", },
+    ]
+  };
+
+  renderFaces = () => {
+    const { faces, } = this.state;
+    return faces.map( face => <li key={face.id}>{face.front}</li>
+  )
+};
 
   render() {
     return (
       <div>
         <h1>flash cards</h1>
+        <ul>
+          { this.renderFaces() }
+        </ul>
       </div>
     );
   }
